621472ce3f2ba33a6e44d17595fa93cb06b37190
Trịnh Gia Bảo <124680653+client100102@users.noreply.github.com>
2023-04-06 21:29:08 +0700

    Add files via upload

---
+++ 2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+using namespace std;
+
+int main()
+{
+    int number, guess, tries = 0;
+    srand(time(0));
+    number = rand() % 100 + 1;
+    cout << "Choi mot tro choi nhe, hay doan mot so tu 1 den 100 nao.\n";
+    do
+    {
+        cout << "So ban doan: ";
+        cin >> guess;
+        tries++;
+
+        if (guess > number)
+            cout << "Hehehe cao qua roi!\n\n";
+        else if (guess < number)
+            cout << "Ui, so cao hon ti nao!\n\n";
+        else
+            cout << "\nChinh xac roi, ban da doan dung sau " << tries << " lan thu !\n" "Do la so: " << guess<< " =D";
+    } while (guess != number);
+
+    return 0;
+}
---
+++ 4.cpp
@@ -0,0 +1,39 @@
+#include <iostream>
+#include<math.h>
+
+using namespace std;
+
+int main()
+{
+	int size = 10;
+	for(int x = 0;x < size; x++)
+	{
+		for(int y = 0; y <= 4 * size; y++)
+		{
+			double dist1 = sqrt(pow(x - size, 2) + pow(y - size, 2));
+			double dist2 = sqrt(pow(x - size, 2) + pow(y - 3 * size, 2));
+			if(dist1 < size + 0.5 || dist2 < size + 0.5)
+			{
+				cout << "b";
+			}
+			else
+			{
+				cout << " ";
+			}
+		}
+		cout << endl;
+	}
+	for(int x = 1; x < 2 * size; x++)
+	{
+		for(int y = 0; y < x; y++)
+		{
+			cout << " ";
+		}
+		for(int y = 0; y < 4 * size + 1 - 2 * x; y++)
+		{
+			cout << "b";
+		}
+		cout << endl;
+	}
+	return 0;
+}
